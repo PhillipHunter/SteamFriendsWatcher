b3fa356ee76659ca0ce63c89384dfbac421e69fe
Phillip Hunter <phunter95@gmail.com>
2016-12-04 17:48:59 -0500

    Disabled navigation for web browser.

--- SteamFriendsWatcher/Main.Designer.cs
+++ SteamFriendsWatcher/Main.Designer.cs
@@ -92,6 +92,8 @@
             // 
             // webBrowser1
             // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
             this.webBrowser1.Location = new System.Drawing.Point(15, 58);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
